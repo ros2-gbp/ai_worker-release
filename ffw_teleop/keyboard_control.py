1424e60fdba8541df7119a3fd558e17427763c4c
Pyo <pyo@robotis.com>
2025-06-04 19:54:24 +0900

    Rebase from 'debian/jazzy/ffw_teleop'

--- ffw_teleop/keyboard_control.py
+++ ffw_teleop/keyboard_control.py
@@ -18,6 +18,7 @@
 
 import tkinter as tk
 
+import numpy as np
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import JointState
@@ -33,7 +34,8 @@ class KeyboardController(Node):
             'arm_l': {
                 'joints': [
                     'arm_l_joint1', 'arm_l_joint2', 'arm_l_joint3',
-                    'arm_l_joint4', 'arm_l_joint5', 'arm_l_joint6', 'arm_l_joint7', 'l_rh_r1_joint'
+                    'arm_l_joint4', 'arm_l_joint5', 'arm_l_joint6', 'arm_l_joint7',
+                    'gripper_l_joint1'
                 ],
                 'positions': [0.0] * 8,
                 'labels': [None] * 8,
@@ -42,12 +44,14 @@ class KeyboardController(Node):
                 'publisher': self.create_publisher(
                     JointTrajectory,
                     '/leader/joint_trajectory_command_broadcaster_left/joint_trajectory',
-                    10)
+                    10),
+                'last_positions': [0.0] * 8
             },
             'arm_r': {
                 'joints': [
                     'arm_r_joint1', 'arm_r_joint2', 'arm_r_joint3',
-                    'arm_r_joint4', 'arm_r_joint5', 'arm_r_joint6', 'arm_r_joint7', 'r_rh_r1_joint'
+                    'arm_r_joint4', 'arm_r_joint5', 'arm_r_joint6', 'arm_r_joint7',
+                    'gripper_r_joint1'
                 ],
                 'positions': [0.0] * 8,
                 'labels': [None] * 8,
@@ -55,25 +59,28 @@ class KeyboardController(Node):
                 'position_step': [0.1] * 8,
                 'publisher': self.create_publisher(
                     JointTrajectory,
-                    '/leader/joint_trajectory_command_broadcaster_right/joint_trajectory', 10)
+                    '/leader/joint_trajectory_command_broadcaster_right/joint_trajectory', 10),
+                'last_positions': [0.0] * 8
             },
-            'neck': {
-                'joints': ['neck_joint1', 'neck_joint2'],
+            'head': {
+                'joints': ['head_joint1', 'head_joint2'],
                 'positions': [0.0] * 2,
                 'labels': [None] * 2,
                 'limits': [(-1.0, 1.0)] * 2,
                 'position_step': [0.1] * 2,
                 'publisher': self.create_publisher(
-                    JointTrajectory, '/neck_controller/joint_trajectory', 10)
+                    JointTrajectory, '/leader/joystick_controller_left/joint_trajectory', 10),
+                'last_positions': [0.0] * 2
             },
-            'body': {
-                'joints': ['linear_joint'],
+            'lift': {
+                'joints': ['lift_joint'],
                 'positions': [0.0],
                 'labels': [None],
                 'limits': [(-1.0, 0.0)],
-                'position_step': [0.1],
+                'position_step': [0.05],
                 'publisher': self.create_publisher(
-                    JointTrajectory, '/body_controller/joint_trajectory', 10)
+                    JointTrajectory, '/leader/joystick_controller_right/joint_trajectory', 10),
+                'last_positions': [0.0]
             }
         }
 
@@ -87,32 +94,81 @@ class KeyboardController(Node):
         self.root = tk.Tk()
         self.root.title('Joint Controller GUI')
         self.hold_buttons = set()
+        self.duration = 1.0  # Duration of each jog in seconds
+        self.num_points = 100  # Number of points in the trajectory
         self.build_gui()
         self.root.after(100, self.process_held_buttons)
+
+        for ctrl in self.controllers.values():
+            ctrl['last_positions'] = list(ctrl['positions'])
 
     def joint_state_callback(self, msg):
         for ctrl_key, ctrl in self.controllers.items():
             for i, joint in enumerate(ctrl['joints']):
                 if joint in msg.name:
-                    # if joint == 'arm_r_joint7' or joint == 'arm_l_joint7':
-                    #     continue
                     idx = msg.name.index(joint)
                     ctrl['positions'][i] = msg.position[idx]
                     if ctrl['labels'][i]:
                         value = msg.position[idx]
                         ctrl['labels'][i].config(text=f'{value:.2f}')
+            # Update last_positions to the latest joint state
+            ctrl['last_positions'] = list(ctrl['positions'])
         self.joint_received = True
+
+    def create_smooth_trajectory(self, joint_names, start_pos, end_pos):
+        traj = JointTrajectory()
+        traj.joint_names = joint_names
+
+        times = np.linspace(0, self.duration, self.num_points)
+
+        for i in range(self.num_points):
+            point = JointTrajectoryPoint()
+            t = times[i]
+
+            t_norm = t / self.duration
+            t_norm2 = t_norm * t_norm
+            t_norm3 = t_norm2 * t_norm
+            t_norm4 = t_norm3 * t_norm
+            t_norm5 = t_norm4 * t_norm
+
+            pos_coeff = 10 * t_norm3 - 15 * t_norm4 + 6 * t_norm5
+            vel_coeff = (30 * t_norm2 - 60 * t_norm3 + 30 * t_norm4) / self.duration
+            acc_coeff = (60 * t_norm - 180 * t_norm2
+                         + 120 * t_norm3) / (self.duration * self.duration)
+
+            positions = []
+            velocities = []
+            accelerations = []
+
+            for j in range(len(joint_names)):
+                pos = start_pos[j] + (end_pos[j] - start_pos[j]) * pos_coeff
+                vel = (end_pos[j] - start_pos[j]) * vel_coeff
+                acc = (end_pos[j] - start_pos[j]) * acc_coeff
+
+                positions.append(pos)
+                velocities.append(vel)
+                accelerations.append(acc)
+
+            point.positions = positions
+            point.velocities = velocities
+            point.accelerations = accelerations
+            point.time_from_start.sec = int(times[i])
+            point.time_from_start.nanosec = int((times[i] % 1) * 1e9)
+
+            traj.points.append(point)
+
+        return traj
 
     def send_command(self, ctrl_key):
         ctrl = self.controllers[ctrl_key]
-        msg = JointTrajectory()
-        msg.joint_names = ctrl['joints']
-        point = JointTrajectoryPoint()
-        point.positions = ctrl['positions']
-        point.time_from_start.sec = 0
-        msg.points.append(point)
+        msg = self.create_smooth_trajectory(
+            ctrl['joints'],
+            ctrl['last_positions'],  # start_pos: previous command
+            ctrl['positions']        # end_pos: new positions (after jog)
+        )
         ctrl['publisher'].publish(msg)
-        self.get_logger().info(f"{ctrl_key} command: {ctrl['positions']}")
+        ctrl['last_positions'] = list(ctrl['positions'])  # update for next jog
+        self.get_logger().info(f"{ctrl_key} smooth command: {ctrl['positions']}")
 
     def change_joint(self, ctrl_key, joint_index, direction):
         ctrl = self.controllers[ctrl_key]
